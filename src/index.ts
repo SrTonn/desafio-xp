180d5d8e464ce2f87dc70614889c2fde992f1e85
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-15 10:51:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'express-async-errors';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+//  routers
+
+//  ErrorMiddleware
+
+app.listen(PORT, () => {
+  console.log(`🔥 Server is running on PORT: ${PORT}`);
+});
