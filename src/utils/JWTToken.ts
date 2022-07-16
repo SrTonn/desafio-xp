3f998908b947ce5eacffa2d34ddafcb48d110bc5
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-15 21:56:44 -0300

    feat: add JWT

---
+++ src/utils/JWTToken.ts
@@ -0,0 +1,23 @@
+import { sign, SignOptions, verify } from 'jsonwebtoken';
+import HttpException from '../shared/http.exception';
+
+const SECRET = process.env.JWT_SECRET!;
+const jwtConfig: SignOptions = {
+  expiresIn: '60m',
+  algorithm: 'HS256',
+};
+
+const generateJWTToken = (payload: string) => sign(payload, SECRET, jwtConfig);
+
+const authenticateToken = async (token: string) => {
+  try {
+    return verify(token, SECRET, jwtConfig);
+  } catch {
+    throw new HttpException(401, 'Invalid token');
+  }
+};
+
+export {
+  generateJWTToken,
+  authenticateToken,
+};
