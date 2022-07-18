dccd9858fce6072bbc27cb5e160284f2ae53c99c
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-18 10:28:35 -0300

    feat: active error middleware

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import 'express-async-errors';
 import 'dotenv/config';
 import routers from './router';
+import httpErrorMiddleware from './middlewares/error';
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routers);
-//  ErrorMiddleware
+app.use(httpErrorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`🔥 Server is running on PORT: ${PORT}`);
