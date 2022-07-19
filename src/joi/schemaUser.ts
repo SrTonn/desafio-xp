13e4f25f8f4d4714beccd2339e3b89102e65a3d3
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-19 10:06:42 -0300

    feat: add new schema

--- src/joi/schemaUser.ts
+++ src/joi/schemaUser.ts
@@ -6,13 +6,20 @@ const schemaLogin = Joi.object().keys({
 });
 
 const schemaNewUser = Joi.object().keys({
+  nickName: Joi.string().min(3),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   firstName: Joi.string().min(3).required(),
   lastName: Joi.string().min(3).required(),
 });
+const schemaUpdateUser = Joi.object().keys({
+  nickName: Joi.string().min(3),
+  email: Joi.string().email(),
+  password: Joi.string().min(6),
+});
 
 export {
   schemaLogin,
   schemaNewUser,
+  schemaUpdateUser,
 };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,10 +2,12 @@ import error from './error';
 import authValidation from './authValidation';
 import userLoginValidation from './userLoginValidation';
 import newUserValidation from './newUserValidation';
+import updateUserValidation from './updateUserValidation';
 
 export {
   error,
   authValidation,
   userLoginValidation,
+  updateUserValidation,
   newUserValidation,
 };
