abe4ddd490382738e816f8a5b44b546e86cf7e00
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-19 10:31:38 -0300

    feat: add new route /wallet

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import accountController from './controllers/account.controller';
+import walletController from './controllers/wallet.controller';
 
 const routers = Router();
 
 routers.use('/account', accountController);
+routers.use('/wallet', walletController);
 
 export default routers;
