0fe7ab1344266c649491460a89034f0a6b7e26b9
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-20 11:50:27 -0300

    feat: add /assets route

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import accountController from './controllers/account.controller';
 import walletController from './controllers/wallet.controller';
 import investmentController from './controllers/investment.controller';
+import assetsController from './controllers/assets.controller';
 
 const routers = Router();
 
 routers.use('/account', accountController);
 routers.use('/wallet', walletController);
 routers.use('/investment', investmentController);
+routers.use('/assets', assetsController);
 
 export default routers;
