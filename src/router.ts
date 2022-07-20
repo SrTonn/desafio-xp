a405597f6b59d52850fbbcca16c4d8c5ccbfaefc
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-19 22:57:21 -0300

    feat: add /investment route

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import accountController from './controllers/account.controller';
 import walletController from './controllers/wallet.controller';
+import investmentController from './controllers/investment.controller';
 
 const routers = Router();
 
 routers.use('/account', accountController);
 routers.use('/wallet', walletController);
+routers.use('/investment', investmentController);
 
 export default routers;
