b4623601d893acd963db8f78242d71d85c22a402
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-24 00:52:31 -0300

    refactor: move files to /src

--- src/__tests__/services/account.test.ts
+++ src/__tests__/services/account.test.ts
@@ -1,37 +1,37 @@
-import { User } from '../../src/database/models/User';
-import * as JWT from '../../src/utils/JWTToken';
-import { authentication, createUser, removeUser, updateUser } from '../../src/services/account.service';
-import { Wallet } from '../../src/database/models/Wallet';
-import { UserStock } from '../../src/database/models/UserStock';
-import HttpException from '../../src/shared/http.exception';
+import { User } from '../../database/models/User';
+import * as JWT from '../../utils/JWTToken';
+import { authentication, createUser, removeUser, updateUser } from '../../services/account.service';
+import { Wallet } from '../../database/models/Wallet';
+import { UserStock } from '../../database/models/UserStock';
+import HttpException from '../../shared/http.exception';
 
 describe('Test account service', () => {
   const mockBodyNewAccount = {
-    firstName: "Stephen",
-    lastName: "Curry",
-    email: "stephen.curry@gmail.com",
-    password: "Curry@Stephen",
+    firstName: 'Stephen',
+    lastName: 'Curry',
+    email: 'stephen.curry@gmail.com',
+    password: 'Curry@Stephen',
   };
 
   const fakeId = 4;
 
   const mockUserFindOneORFindPK = {
-    nickName: "little Ol",
-    firstName: "Ollie",
-    lastName: "Bryant",
-    email: "ollie.bryant@gmail.com"
-  }
+    nickName: 'little Ol',
+    firstName: 'Ollie',
+    lastName: 'Bryant',
+    email: 'ollie.bryant@gmail.com',
+  };
 
   const mockUserResponse = {
     id: fakeId,
     nickName: undefined,
-    firstName: "Stephen",
-    lastName: "Curry",
-    email: "stephen.curry@gmail.com",
-    password: "Curry@Stephen",
+    firstName: 'Stephen',
+    lastName: 'Curry',
+    email: 'stephen.curry@gmail.com',
+    password: 'Curry@Stephen',
     updatedAt: '2022-07-22T13:27:01.183Z',
     createdAt: '2022-07-22T13:27:01.183Z',
-    toJSON() {}
+    toJSON() {},
   };
 
   const mockWalletResponse = { balance: 0, userId: fakeId };
@@ -55,47 +55,50 @@ describe('Test account service', () => {
     expect(response).toEqual({ token: fakeToken });
   });
 
-  it('Será validado que não é possível fazer login com dados de uma conta inexistente', async () => {
-    jest.spyOn(User, 'findOne').mockResolvedValue(null as never);
-    jest.spyOn(JWT, 'generateJWTToken').mockReturnValue(fakeToken);
+  it(
+    'Será validado que não é possível fazer login com dados de uma conta inexistente',
+    async () => {
+      jest.spyOn(User, 'findOne').mockResolvedValue(null as never);
+      jest.spyOn(JWT, 'generateJWTToken').mockReturnValue(fakeToken);
 
-    let response = {} as HttpException;
+      let response = {} as HttpException;
 
-    try {
-      await authentication({ email: 'usuario.inexistente@gmail.com', password: 'senha12345' });
-    } catch (error) {
-      response = error as HttpException;
-    }
+      try {
+        await authentication({ email: 'usuario.inexistente@gmail.com', password: 'senha12345' });
+      } catch (error) {
+        response = error as HttpException;
+      }
 
-    expect(response.status).toBe(400);
-    expect(response.message).toBe('Invalid fields');
-  });
+      expect(response.status).toBe(400);
+      expect(response.message).toBe('Invalid fields');
+    },
+  );
 
   it('Será validado que é possível fazer update do apelido do usuário', async () => {
-
     jest.spyOn(User, 'update').mockResolvedValue([1]);
     jest.spyOn(User, 'findByPk').mockResolvedValue(mockUserFindOneORFindPK as User);
-    const response = await updateUser(fakeId, { nickName: "little Ol" });
+    const response = await updateUser(fakeId, { nickName: 'little Ol' });
 
     expect(response).toEqual(mockUserFindOneORFindPK);
   });
 
   it('Será validado não é possível remover um usuário com saldo acima de 0', async () => {
     const mockResponse = {
-      nickName: "little Ol",
-      firstName: "Ollie",
-      lastName: "Bryant",
-      email: "ollie.bryant@gmail.com",
-    }
+      nickName: 'little Ol',
+      firstName: 'Ollie',
+      lastName: 'Bryant',
+      email: 'ollie.bryant@gmail.com',
+    };
     const mockUserStock = {
       userId: fakeId,
       stockCode: 'AGRO3',
       availableQuantity: 1,
-      investedAmount: 23.24
-    }
+      investedAmount: 23.24,
+    };
     jest.spyOn(User, 'findByPk').mockResolvedValue(mockResponse as User);
-    jest.spyOn(Wallet, 'findByPk').mockResolvedValue({ userId: fakeId, balance: 9929.11 } as Wallet);
     jest.spyOn(UserStock, 'findOne').mockResolvedValue(mockUserStock as UserStock);
+    jest.spyOn(Wallet, 'findByPk')
+      .mockResolvedValue({ userId: fakeId, balance: 9929.11 } as Wallet);
 
     let response = {} as HttpException;
     try {
@@ -107,5 +110,4 @@ describe('Test account service', () => {
     expect(response.status).toBe(406);
     expect(response.message).toBe('Account with available balance');
   });
-
 });
