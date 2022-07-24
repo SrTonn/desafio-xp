2be2ffdaa93e0c50d05c242c039b211c2ce57e0d
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-24 14:01:56 -0300

    refactor: assets controller

--- src/controllers/assets.controller.ts
+++ src/controllers/assets.controller.ts
@@ -6,7 +6,7 @@ import { getAssets, updateAssets } from '../services/assets.service';
 const assetsRouter = Router();
 
 assetsRouter
-  .get('/update', authValidation, async (req: Request, res: Response) => {
+  .get('/update', authValidation, async (_req: Request, res: Response) => {
     await updateAssets();
     return res.sendStatus(200);
   });
@@ -21,7 +21,7 @@ assetsRouter
   .get('/:stockCode', authValidation, async (req: Request, res: Response) => {
     const { stockCode } = req.params;
     const response = await getAssets(stockCode);
-    return res.status(201).json(response);
+    return res.status(200).json(response);
   });
 
 export default assetsRouter;
