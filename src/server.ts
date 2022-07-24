11ea1ea4d4eab6d02791d1065e10b4928c753531
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-24 00:35:23 -0300

    refactor: use import from app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,6 @@
-/* eslint-disable no-console */
-import express from 'express';
-import 'express-async-errors';
+import app from './app';
 import 'dotenv/config';
-import routers from './router';
-import httpErrorMiddleware from './middlewares/error';
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(express.json());
-app.use(routers);
-app.use(httpErrorMiddleware);
-
-app.listen(PORT, () => {
-  console.log(`🔥 Server is running on PORT: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🔥 Server is running on PORT: ${PORT}`));
