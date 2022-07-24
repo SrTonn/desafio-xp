b56a1b81a56fb0ebc37267fac25d376a3595a601
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-24 00:34:36 -0300

    feat: create app.ts

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import 'express-async-errors';
+import routers from './router';
+import httpErrorMiddleware from './middlewares/error';
+
+const app = express();
+
+app.use(express.json());
+app.use(routers);
+app.use(httpErrorMiddleware);
+
+export = app;
