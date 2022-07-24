91b8d88a0a871c9d2787edcadfdd1928e3625bb6
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-23 22:34:09 -0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ export default {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   testRegex: './*\\.test\\.ts$',
+  collectCoverageFrom: [
+    "src/{!(database|joi|utils|middlewares|shared),}/*.ts"
+  ],
   testTimeout: 180000,
   maxWorkers: 1,
 };
