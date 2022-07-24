90fa8cd4e94c6c041e943242ba95ac4f66831ceb
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-24 14:01:21 -0300

    test: use stockList mock

--- src/__tests__/services/assets.test.ts
+++ src/__tests__/services/assets.test.ts
@@ -1,21 +1,22 @@
 import { Stocks } from '../../database/models/Stocks';
 import { updateAssets, getAssets } from '../../services/assets.service';
+import * as Mock from '../mocks';
 
 describe('Test assets service', () => {
   it('Será validado que é possível listar uma ação ao buscar pelo código!!!', async () => {
     jest.spyOn(Stocks, 'findOne').mockResolvedValue(Mock.stockList[0] as never);
 
-    const response = await getAssets(mockStockList[0].stock);
-    expect(response).toEqual(mockStockList[0]);
+    const response = await getAssets(Mock.stockList[0].stock);
+    expect(response).toEqual(Mock.stockList[0]);
   });
 
   it(
     'Será validado que é possível listar todas ações disponíveis no mercado com sucesso',
     async () => {
-      jest.spyOn(Stocks, 'findAll').mockResolvedValue(mockStockList as never);
+      jest.spyOn(Stocks, 'findAll').mockResolvedValue(Mock.stockList as never);
 
       const response = await getAssets();
-      expect(response).toEqual(mockStockList);
+      expect(response).toEqual(Mock.stockList);
     },
   );
 
